c512a200cee359f1c748e7545292f1831cbb763d
FaYeest <mtbfarrasandi@gmail.com>
2024-10-21 08:11:07 +0700

    added some file and code

---
+++ 1to100.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+    // loop 1 to 100
+    for (i=1;i <= 100;i++) {
+        printf("%d\n", i); // print current number
+    }
+    return 0; // to close the program
+}
---
+++ while.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// while loop
+int main() {
+    int i = 2;
+
+    while(i<20) {
+        printf("Hello World\n");
+        i++;
+    }
+}
